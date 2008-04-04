4fb6f4e91298c3ca0e8aa2d87b9a300257c3cada
sdfisk <sdfisk@8677164b-3244-0410-a105-c5629bf06332>
2008-04-04 07:22:19 +0000

    added arduino source and arduino java class.  At some point i would like to move the old groups code out of the trunk

---
+++ trunk/nyoron/Arduino.java
@@ -0,0 +1,395 @@
+import gnu.io.*;
+import java.util.Enumeration;
+import java.util.TooManyListenersException;
+import java.lang.NumberFormatException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.util.concurrent.locks.*;
+import java.util.Scanner;
+
+
+/**
+
+IRarduino.java
+This code manages the connection to the IR sensor connected to an arduino
+board. Additionally, said board will be connected at some point to Push-button
+sensors, and said information will need to be collected from it.
+
+	<h1>Revision History:</h1>
+	<ul>
+		<li>March 28, 2008, Benjamin Gauronskas</li>
+		<ul>
+			<li>Rewrote the code so that it would work with the robot.</li>
+			<li>Fixed bugs in output.</li>
+			<li>Added comments.</li>
+		</ul>
+		<li>March ??, 2008, Joyce Tang</li>
+		<ul>
+			<li>Initial version.</li>
+		</ul>
+	</ul>
+
+
+  @author                      Joyce Tang
+  @version                     0.2
+*/
+public class Arduino implements Runnable, SerialPortEventListener {
+
+
+/*
+The arduino should probably work like so,
+buttonsOn is a boolean that defaults to true.
+sent is a boolean that defaults to false.
+
+if a push button sensor is pressed and buttonsOn == true and sent == false
+	send a one to output.
+	set some boolean sent = true;
+if a push button sensor  depressed and buttonsOn == true and sent == true
+	set some boolean sent = false;
+if a stop code (say a byte 0x01) is received
+	set boolean "buttonsOn" = false
+if a start code (say a byte 0x02) is received
+	set boolean "buttonsOn" = true
+if a IRget code (say a byte 0x03) is received
+	send the reading from the IR sensor.
+
+
+*/
+
+	/**
+	Message to stop bump sensors.
+	*/
+	//public static final byte BUMPER_STOP	= 'a';
+	/**
+	Message to start bump sensors
+	*/
+	//public static final byte BUMPER_START	= 'b';
+	/**
+	Message to get Infrared reading.
+	*/
+	//public static final byte IR_GET			= 'c';
+	/**
+	Message to get touch reading.
+	*/
+	//public static final byte TOUCH_GET		= 't';
+	/**
+	How long Arduino needs to send information to the
+	*/
+	public static final int ARDUINO_SLEEP	= 150;
+	/**
+	Message to get arduino data.
+	*/
+	public static final byte GET_DATA	= 'd';
+	/**
+	The size of a buffer used to receive info.
+	*/
+	public static final int BUFFER_SIZE		= 10;
+	/**
+	The size of a buffer used to receive info.
+	*/
+	public static final int BUMPSENSOR_COUNT= 1;
+	/**
+	Touch sensor reading.
+	*/
+	public boolean[] isBumped;
+	/**
+	IR Range Finder reading.
+	*/
+	public int IRSensorVal;
+	/**
+	The stream from which we receive information from the arduino board.
+	*/
+	private InputStream in;
+
+	/**
+	The stream to which we write information to the Arduino board
+	*/
+	private OutputStream out;
+	/**
+	The port to which the Arduino board is connected.
+	*/
+	private SerialPort serialPort;
+	/**
+	A thread that spins busily while waiting for information from the arduino
+	board.
+	*/
+	private Thread readThread;
+
+	/**
+	This lock keeps the channels mutually exclusive
+	*/
+	private ReentrantLock dataLock;
+
+	/**
+	Tests the functionality of the infrared and touch sensors
+
+	@param		args		These are ignored
+	@author		Benjamin Gauronskas
+    */
+	public static void main(String[] args) {
+		Arduino arduino = new Arduino("COM5");
+		while(true){
+			System.out.println(arduino.getIRSensor());
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {}
+		}
+	}
+
+	/**
+	Constructor for the sensor object
+
+	@param		comPort		A human readable name for the port which the
+							device is connected to.
+	@author		Benjamin Gauronskas
+    */
+	public Arduino(String comPort) {
+		IRSensorVal = 0;
+		isBumped = new boolean[BUMPSENSOR_COUNT];
+
+		dataLock = new ReentrantLock();
+
+		Enumeration portList = CommPortIdentifier.getPortIdentifiers();
+
+
+		//This voodoo cycles through all com ports to find com 4.
+		CommPortIdentifier channelId =
+			(CommPortIdentifier) portList.nextElement();
+		while (portList.hasMoreElements() &&
+			!(channelId.getPortType() == CommPortIdentifier.PORT_SERIAL &&
+			channelId.getName().equals(comPort))) {
+			channelId =	(CommPortIdentifier) portList.nextElement();
+		}
+
+		//Connect to the port, God Knows what this voodoo is.
+		//I think the magical 2000 is the timeout period.
+		try {
+			serialPort = (SerialPort) channelId.open("irRead", 2000);
+		} catch (PortInUseException e) {}
+
+		//Allows us to read from the serial port.
+		try {
+			in = serialPort.getInputStream();
+			out = serialPort.getOutputStream();
+			serialPort.setOutputBufferSize(20);
+		} catch (IOException e) {}
+
+		//This is the one piece of code that interests me...
+		//figure out how to EXPLOIT this technique.
+		//To temporarily remove event listener,
+		//we use the aptly named method removeEventListener()
+		//it takes no parameters, and throws no exceptions.
+		try {
+			serialPort.addEventListener(this);
+		} catch (TooManyListenersException e) {}
+
+		//I imagine this has to do with the above.
+		//We need to compartmentalize this.
+		//serialPort.notifyOnDataAvailable(true);
+
+		try {
+			serialPort.setSerialPortParams(	9600,
+											SerialPort.DATABITS_8,
+											SerialPort.STOPBITS_1,
+											SerialPort.PARITY_NONE);
+		} catch (UnsupportedCommOperationException e) {}
+
+		readThread = new Thread(this);
+		readThread.start();
+	}
+
+	/**
+	An infinite loop that will be interrupted and then sensor information
+	will be taken care of in a seperate thread.
+
+
+	@author		Joyce Tang
+    */
+	public void run() {
+		
+		//Serial string reading.
+		String sensorValue = "";
+		String[] sensorSplit;
+
+		byte[] arduinoRead = new byte[BUFFER_SIZE];
+		int bumpValue = 0;
+		int index = 0;
+
+		while(true){
+
+
+			try{
+				out.write(GET_DATA);
+				out.flush();
+
+				Thread.sleep(ARDUINO_SLEEP);
+				in.read(arduinoRead);
+				//in.read(touchSensorReading);
+
+
+				if(arduinoRead != null){
+					sensorValue = (new String(arduinoRead)).trim();
+				}
+				else{
+					sensorValue = "-1,-1";
+				}
+			}
+			catch (IOException ex) {}
+			catch (InterruptedException e) {}
+			
+
+			sensorSplit = sensorValue.split(",");
+			
+			if(!(sensorSplit[0].equals("-1")) &&
+				!(sensorSplit[0].trim().equals(""))){
+
+				dataLock.lock();
+				
+				IRSensorVal = Integer.parseInt(sensorSplit[0]);
+
+				dataLock.unlock();
+			}
+			
+			for(index = 1; index < sensorSplit.length; index++){
+				if(sensorSplit[index].equals("1")){
+					new BumpPressed(index);
+				}
+			}
+	
+
+			try {
+				Thread.sleep(ARDUINO_SLEEP);
+			} catch (InterruptedException e) {}
+		}
+
+	}
+
+
+	/**
+	gets the IR reading
+
+	@author		Benjamin Gauronskas
+    */
+	public int getIRSensor(){
+		int returnValue;
+		dataLock.lock();
+		returnValue = IRSensorVal;
+		dataLock.unlock();
+
+		return returnValue;
+	}
+
+	/**
+	The interrupt for when data is available.
+
+	@param		event		The happening that triggered the interrupt.
+
+	@author		Joyce Tang
+    */
+	public void serialEvent(SerialPortEvent event) {
+		byte[] readBuffer;
+		byte nextChar = '0';
+
+		switch (event.getEventType()){
+			case SerialPortEvent.BI:
+			case SerialPortEvent.OE:
+			case SerialPortEvent.FE:
+			case SerialPortEvent.PE:
+			case SerialPortEvent.CD:
+			case SerialPortEvent.CTS:
+			case SerialPortEvent.DSR:
+			case SerialPortEvent.RI:
+			case SerialPortEvent.OUTPUT_BUFFER_EMPTY: break;
+
+			//This is the only thing we are looking for.
+			case SerialPortEvent.DATA_AVAILABLE:
+
+				//System.out.println("Interrupted.");
+				readBuffer = new byte[BUFFER_SIZE];
+				int numBytes = 0;
+
+				try {
+					int i = 0;
+					while (nextChar != '\n' && i < BUFFER_SIZE) {
+						//Get rid of whatever is in the buffer
+						nextChar = (byte)in.read();
+						readBuffer[i] = nextChar;
+						i++;
+					}
+
+					System.out.println((new String(readBuffer)).trim());
+
+					//Insert logic to handle the bump... probably back up
+					//then rotate and drive again.
+				} catch (IOException e) {}
+
+			default: break;
+		}
+	}
+
+	/**
+	 * <p>Sweep thread keeps running the sweep temp method over and over in
+	 * order to collect more temperatures.</p>
+	 *
+	 * @author			Benjamin Gauronskas
+	 * @version			0.1
+	 */
+	private static class BumpPressed implements Runnable
+	{
+
+		public Thread t;
+		public int bumpNumber;
+
+
+		// Constructor initilized vars
+		/**
+		 Constructor takes bump Pressed.
+		 
+		 @param	bumpNumber		Sensor triggered.
+		 @author			Benjamin Gauronskas
+		 */
+		public BumpPressed(int bumpNumber)
+		{
+
+			this.bumpNumber = bumpNumber;
+			t = new Thread(this,"button " + bumpNumber);
+			t.start();
+		}
+
+		/**
+		 What to do when a button is pressed
+		 
+		 
+		 @author			Benjamin Gauronskas
+		 */
+		public void run()
+		{
+			System.out.println("Bump number " + bumpNumber);
+		}
+	}
+
+
+}
+
+
+
+/*
+The arduino should probably work like so,
+buttonsOn is a boolean that defaults to true.
+sent is a boolean that defaults to false.
+
+if a push button sensor is pressed and buttonsOn == true and sent == false
+	send a one to output.
+	set some boolean sent = true;
+if a push button sensor  depressed and buttonsOn == true and sent == true
+	set some boolean sent = false;
+if a stop code (say a byte 0x01) is received
+	set boolean "buttonsOn" = false
+if a start code (say a byte 0x02) is received
+	set boolean "buttonsOn" = true
+if a IRget code (say a byte 0x03) is received
+	send the reading from the IR sensor.
+
+
+*/
---
+++ trunk/nyoron/arduino/applet/arduino.cpp
@@ -0,0 +1,133 @@
+#include "WProgram.h"
+
+//These are codes that will be recieved via UART from the robot.
+/* Range in centimeters = (val * 1,24) */
+
+ /*
+ I, Benjamin, would appreciate code that would function like this:
+The arduino should probably work like so,
+buttonsOn is a boolean that defaults to true.
+sent is a boolean that defaults to false.
+
+if a push button sensor is pressed and buttonsOn == true and sent == false
+	send a one to output.
+	set some boolean sent = true;
+if a push button sensor  depressed and buttonsOn == true and sent == true
+	set some boolean sent = false;
+if a stop code (say a byte 0x01) is received
+	set boolean "buttonsOn" = false
+if a start code (say a byte 0x02) is received
+	set boolean "buttonsOn" = true
+if a IRget code (say a byte 0x03) is received
+	send the reading from the IR sensor.
+*/
+
+//There is a possibility we don't need this include
+#include <SoftwareSerial.h>
+
+//These are codes that will be recieved via UART from the robot.
+
+#define GET_DATA 'd'
+#define DATA_DELIMITER ','
+
+void setup();
+void loop();
+int irRange = 0;       // signal pin ANALOG-0 for Range finder
+int bump0 = 7;               // signal pin DIGITAL-1 for bump sensor
+int irRangeData = 0;
+int bumpData = 0;
+int BAUD = 9600;
+int rangeCM = 0; 
+
+//The value read from the serial connection.
+char input;
+
+
+void setup() {
+
+  input = 0;
+  beginSerial(BAUD);
+  pinMode(irRange, OUTPUT);  
+  pinMode(bump0, OUTPUT);  
+  //mySerial.begin(BAUD);
+  
+}
+
+void loop() {
+
+  //Check if a command has been sent.
+  if (Serial.available() > 0 ){
+    input =  Serial.read();
+  }
+  else{
+    input = 0;
+  }
+  
+
+  //Handle the command.
+  //This might be better if re-written as a switch
+  //statement.
+  
+  /*
+  if(input == BUMPER_STOP){
+    buttonsOn = false;
+  }
+  else if (input == BUMPER_START){
+    buttonsOn = true;
+  }
+  else if (input == IR_GET){
+    irRangeValue = analogRead(irRangeSignal); 
+    Serial.print(irRangeValue);
+    printByte(10);
+    Serial.flush();
+  }
+  else if (input == TOUCH_GET){
+      touchRangeValue = analogRead(touchRangeSignal); 
+      Serial.print(touchRangeValue);
+      printByte(10);
+      Serial.flush();
+  }
+  else if (input == -1){
+  }
+
+  */
+  if (input == GET_DATA){
+    irRangeData = analogRead(irRange);
+    bumpData = digitalRead(bump0);
+    // equation for range.  NOT ACCURATE PLOT IN ECXCEL
+    rangeCM = (2914 / (irRangeData + 5)) - 1;
+   
+     // send range value converted to CM
+    Serial.print(rangeCM);
+     // send delimiter to parse in java
+    Serial.print(DATA_DELIMITER);
+     // send bump sensor status 0 or 1
+    Serial.print(bumpData);
+    
+ 
+    printByte(10);
+    Serial.flush();
+  }
+  
+  //These two delays are in the original code.
+  //I think they should wrap around where we get information.
+  //IE, they 50 millisecond wait should be moved after the
+  //get from the IR sensor, and the 100 millisecond wait
+  //should be moved after the send to the Channel.
+  delay(50);    
+ 
+  delay(100);    
+}
+
+int main(void)
+{
+	init();
+
+	setup();
+    
+	for (;;)
+		loop();
+        
+	return 0;
+}
+
