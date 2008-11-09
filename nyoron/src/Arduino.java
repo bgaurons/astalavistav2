9d9311952defa5fde280f7dd0b19e247bd2992da
sdfisk <sdfisk@8677164b-3244-0410-a105-c5629bf06332>
2008-11-09 23:54:27 +0000

    reworked sensor access and added Sonar wrapper class

--- nyoron/src/Arduino.java
+++ nyoron/src/Arduino.java
@@ -4,54 +4,43 @@ This class is pretty good
 
 import java.io.*;
 import java.util.*;
+
 import gnu.io.*;
 
-public class Arduino
+/**
+ * @author Scott Fisk
+ *
+ */
+public class Arduino implements Runnable, SerialPortEventListener
 {  
 	/**
-	Message to get sonar0 reading.
+	Change how many data points to save when performing averages.
 	*/
-	public static final byte FRONTSONAR_GET			= '0';
-
-	/**
-	Message to get sonar1 reading.
-	*/	
-	public static final byte LEFTSONAR_GET			= '1';
+	static final int HISTORY_DATA_POINTS			= '5';
 	
 	/**
-	Message to get sonar2 reading.
-	*/
-	public static final byte RIGHTSONAR_GET			= '2';
+	Raw data coming from arduino
+	 */
+	private StringBuffer readBuffer = new StringBuffer(20);
 	
-	/**
-	Message to get compass reading.
-	*/
-	public static final byte COMPASS_GET		= 'c';
+	/** 
+	most recent readings of datas from arduino 
+	 */
+	private int sonarFrontVal[] = new int[HISTORY_DATA_POINTS];
+	private int sonarLeftVal[] = new int[HISTORY_DATA_POINTS];
+	private int sonarRightVal[] = new int[HISTORY_DATA_POINTS];
+	private float compassVal[] = new float[HISTORY_DATA_POINTS];
 	
-	/**
-	Front sonar sensor reading.
-	*/
-	private int sonarFrontVal;
+	/** 
+	radian values of angle and distance offsets of sensors from center of robot 
+	 */
+	public static double sonarLeftAngle = (3*Math.PI)/2.0;
+	public static double sonarRightAngle = Math.PI;
+	public static double sonarFrontAngle = 0;
 	
-	/**
-	Left sonar sensor reading.
-	*/
-	private int sonarLeftVal;
-	
-	/**
-	Right sonar sensor reading.
-	*/
-	private int sonarRightVal;
-	
-	/**
-	Compass sensor reading.
-	*/
-	private float compassVal;
-	
-	
-	public static int sonarLeftAngle = 270;
-	public static int sonarRightAngle = 90;
-	public static int sonarFrontAngle = 0;
+	public static int sonarLeftDistance = 9;
+	public static int sonarRightDistance = 9;
+	public static int sonarFrontDistance = 9;
 	
 	/**
 	The input data stream from the arduino board.
@@ -59,42 +48,45 @@ public class Arduino
 	private InputStream in;
 	
 	/**
-	The stream to which we write information to the Arduino board
-	*/
-	private OutputStream out;
-	
-	/**
 	The port to which the Arduino board is connected.
 	*/
 	private SerialPort serialPort;
-
+	
+	/**
+	The thread which will constantly read arduino data
+	*/
+	Thread readThread;
 
 	/**
 	Tests the functionality of the sonar and compass sensor
 
 	@param		args		These are ignored
-	@author		Scott Fisk & Benjamin Gauronskas
+	@author		Scott Fisk
     */
 	public static void main(String[] args) {
 		Arduino arduino = new Arduino("COM6");
 		while(true){
 			try {
+				System.out.println(arduino.getRightSonar());
+				System.out.println(arduino.getLeftSonar());
+				System.out.println(arduino.getFrontSonar());
 				System.out.println(arduino.getCompass());
-				Thread.sleep(100);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {}
 		}
 	}
 
+	/**
+	Constructor to initalize the arduino serial port and then start the arduino thread.
 
+	@param		comPort 	pass a comport of the arduino
+	@author		Scott Fisk
+    */
 	public Arduino(String comPort) {
-		compassVal = 0;
-		sonarFrontVal = 0;
-		sonarLeftVal = 0;
-		sonarRightVal = 0;
 		
 		Enumeration portList = CommPortIdentifier.getPortIdentifiers();
 
-		
+		// earth to find the comport
 		CommPortIdentifier channelId =
 			(CommPortIdentifier) portList.nextElement();
 		while (portList.hasMoreElements() &&
@@ -112,9 +104,16 @@ public class Arduino
 	   
 	      try {
 	         in = serialPort.getInputStream();
-	         out = serialPort.getOutputStream();
 	      } catch (IOException e) {}
 	      
+	      try {
+			serialPort.addEventListener(this);
+	      } catch (TooManyListenersException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+	      }
+	      
+	      serialPort.notifyOnDataAvailable(true);
 	      
 	      try {
 	         // set port parameters
@@ -124,14 +123,8 @@ public class Arduino
 	      } catch (UnsupportedCommOperationException e) {}
 	      
 	      // start the read thread
-	      //readThread = new Thread(this);
-	      //readThread.start();
-	      try {
-			Thread.sleep(1300);
-		} catch (InterruptedException e) {
-	      	// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	      readThread = new Thread(this);
+	      readThread.start();
 	      
 	   }
 	
@@ -142,30 +135,9 @@ public class Arduino
     */
 	public float getRightSonar(){
 		float returnValue = 0;
-		byte[] readBuffer = new byte[10];
-
-		try {
-			out.write(RIGHTSONAR_GET);
-			out.flush();
-			Thread.sleep(30);
-				
-           	in.read(readBuffer);
-
-           	String result  = new String(readBuffer);
-            
-            if ( result.trim().length () != 0 ){
-            	sonarRightVal = Integer.parseInt(result);	
-            }
-            else{
-            	System.out.println("Trim 0 string");
-            	sonarRightVal = 0;
-			}
-
-		}
-		catch (IOException ex) {}
-		catch (InterruptedException e) {}
 		
-		returnValue = sonarRightVal;
+		// consult history to obtain a more accurate reading
+		returnValue = averageHistory(sonarRightVal);
 		
 		return returnValue;
 	}
@@ -177,30 +149,9 @@ public class Arduino
     */
 	public float getLeftSonar(){
 		float returnValue = 0;
-		byte[] readBuffer = new byte[10];
-
-		try {
-			out.write(LEFTSONAR_GET);
-			out.flush();
-			Thread.sleep(30);
-				
-           	in.read(readBuffer);
-
-           	String result  = new String(readBuffer);
-            
-            if ( result.trim().length () != 0 ){
-            	sonarLeftVal = Integer.parseInt(result);	
-            }
-            else{
-            	System.out.println("Trim 0 string");
-            	sonarLeftVal = 0;
-			}
-
-		}
-		catch (IOException ex) {}
-		catch (InterruptedException e) {}
 		
-		returnValue = sonarLeftVal;
+		// consult history to obtain a more accurate reading	
+		returnValue = averageHistory(sonarLeftVal);
 		
 		return returnValue;
 	}
@@ -212,35 +163,13 @@ public class Arduino
     */
 	public float getFrontSonar(){
 		float returnValue = 0;
-		byte[] readBuffer = new byte[10];
 
-		try {
-			out.write(FRONTSONAR_GET);
-			out.flush();
-			Thread.sleep(30);
-				
-           	in.read(readBuffer);
-
-           	String result  = new String(readBuffer);
-            
-            if ( result.trim().length () != 0 ){
-            	sonarFrontVal = Integer.parseInt(result);	
-            }
-            else{
-            	System.out.println("Trim 0 string");
-            	sonarFrontVal = 0;
-			}
-
-		}
-		catch (IOException ex) {}
-		catch (InterruptedException e) {}
-		
-		returnValue = sonarFrontVal;
+		// consult history to obtain a more accurate reading
+		returnValue = averageHistory(sonarFrontVal);
 		
 		return returnValue;
 	}
 
-	
 
 	/**
 	gets the compass reading
@@ -249,33 +178,152 @@ public class Arduino
     */
 	public float getCompass(){
 		float returnValue = 0;
-		byte[] readBuffer = new byte[10];
-
-		try {
-			out.write(COMPASS_GET);
-			out.flush();
-			Thread.sleep(30);
-				
-           	in.read(readBuffer);
-
-           	String result  = new String(readBuffer);
-            
-            if ( result.trim().length () != 0 ){
-            	compassVal = Float.parseFloat(result);	
-            }
-            else{
-            	System.out.println("Trim 0 string");
-            	compassVal = 0;
-			}
-
-		}
-		catch (IOException ex) {}
-		catch (InterruptedException e) {}
 		
-		returnValue = compassVal;
+		// consult history to obtain a more accurate reading
+		returnValue = averageHistory(compassVal);
 		
 		return returnValue;
 	}
 
-}
+	/**
+	Averages an array of ints. 
+	
+	@param		sonarLocal 	Pass the array of sonar data to be averaged.
+	@author		Scott Fisk
+    */
+	private float averageHistory(int[] sonarLocal) {
+		int i = 0;
+		float sum = 0;
+		float average = 0;
+		
+		// perform average
+		for (;i<HISTORY_DATA_POINTS;i++){
+			sum += sonarLocal[i];
+		}
+		average = sum/HISTORY_DATA_POINTS;
+		
+		return average;
+	}
 
+	/**
+	Averages an array of floats. 
+	
+	@param		compassLocal 	Pass the array of sonar data to be averaged.
+	@author		Scott Fisk
+    */
+	private float averageHistory(float[] compassLocal) {
+		int i = 0;
+		float sum = 0;
+		float average = 0;
+		
+		// perform average
+		for (;i<HISTORY_DATA_POINTS;i++){
+			sum += compassLocal[i];
+		}
+		average = sum/HISTORY_DATA_POINTS;
+		
+		return average;
+	}
+
+
+	/**
+	Starts a sleeping thread to handle interrupts 
+	
+	@author		Scott Fisk
+    */
+	public void run() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+
+
+	/**
+	Handles serial events when data is available. 
+	
+	@param		event 	Serial port event.
+	@author		Scott Fisk
+    */
+	public void serialEvent(SerialPortEvent event) {
+		switch (event.getEventType()) {
+			case SerialPortEvent.DATA_AVAILABLE:
+				// we get here if data is available on serial port
+				readSerial();
+		}
+	}
+	
+	/**
+	reads a raw serial line until a new line character
+
+	@author		Scott Fisk
+    */
+	private void readSerial() {
+		// variable to store the serial data char by char to check for new line
+		int nextChar;
+		try {
+			// loop until newline
+			while((nextChar=in.read()) != '\n'){
+				if(nextChar != '\r')
+					readBuffer.append((char)nextChar);
+			}
+//			System.out.println(readBuffer);
+			parseBuffer(readBuffer.toString());
+			readBuffer.setLength(0);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	Parses the buffer file and then adds to history 
+	
+	@param		readBuffer 	Raw data read off of serial from arduino
+	@author		Scott Fisk
+    */
+	private void parseBuffer(String readBuffer) {
+		String[] splitS = readBuffer.split(" ");
+		int sonarFront = 0;
+		int sonarLeft = 0;
+		int sonarRight = 0;
+		float compass = 0;
+		
+		sonarFront = Integer.parseInt(splitS[0]);
+		sonarLeft = Integer.parseInt(splitS[1]);
+		sonarRight = Integer.parseInt(splitS[2]);
+		compass = Float.parseFloat(splitS[3]);
+		
+		addToHistory(sonarFront, sonarLeft, sonarRight, compass);
+	}
+
+	/**
+	Adds all sensor data to a history of user specified points.  
+	
+	@param		sonarFront 	Front sonar data value to be added to history
+	@param		sonarLeft 	Left sonar data value to be added to history
+	@param		sonarRight 	Right sonar data value to be added to history
+	@param		compass 	compass data value to be added to history
+	@author		Scott Fisk
+    */
+	private void addToHistory(int sonarFront, int sonarLeft, int sonarRight,
+			float compass) {
+		int i = 0;
+		// shift old data up
+		for (;i<HISTORY_DATA_POINTS-1;i++){
+			sonarFrontVal[i+1] = sonarFrontVal[i];
+			sonarLeftVal[i+1] = sonarLeftVal[i];
+			sonarRightVal[i+1] = sonarRightVal[i];
+			compassVal[i+1] = compassVal[i]; 
+		}
+
+		// add new data to most recent part of array
+		sonarFrontVal[0] = sonarFront;
+		sonarLeftVal[0] = sonarLeft;
+		sonarRightVal[0] = sonarRight;
+		compassVal[0] = compass; 
+	}
+	
+}
---
+++ nyoron/src/Sonar.java
@@ -0,0 +1,85 @@
+/**
+ * 	Simple wrapper class for accessing arduino sonar data
+ */
+
+/**
+ * @author Scott Fisk
+ *
+ */
+public class Sonar {
+
+	/**
+	the distance from center of robot
+	*/
+	public int dist_offset;
+	
+	/**
+	the angle from center of robot
+	*/
+	public double angle_offset;
+	
+	/**
+	which direction the sensor is facing in laymans
+	*/
+	private int orientation = 0;
+	
+	/**
+	Constructor to initalize the determine which globals are relevant
+
+	@param		orientation 	pass the orientation of the sensor you are accessing
+	@author		Scott Fisk
+    */
+	public Sonar(int orientation) {
+		switch(orientation) {
+			// front
+			case 0: 
+				this.orientation = orientation;
+				dist_offset = Arduino.sonarFrontDistance;
+				angle_offset = Arduino.sonarFrontAngle;
+				break;
+			// left
+			case 1:
+				this.orientation = orientation;
+				dist_offset = Arduino.sonarFrontDistance;
+				angle_offset = Arduino.sonarFrontAngle;
+				break;
+			// right
+			case 2:
+				this.orientation = orientation;
+				dist_offset = Arduino.sonarFrontDistance;
+				angle_offset = Arduino.sonarFrontAngle;
+				break;
+			default:
+				break;
+		}
+	}
+	
+	/**
+	Returns the value of the sonar sensor
+
+	@author		Scott Fisk
+    */
+	public float getMeasurement(){
+		float returnValue = 0;
+		
+		switch(orientation) {
+			// front
+			case 0:
+				returnValue = Registers.arduino.getFrontSonar();
+				break;
+			// left
+			case 1:
+				returnValue = Registers.arduino.getLeftSonar();
+				break;
+			// right
+			case 2:
+				returnValue = Registers.arduino.getLeftSonar();
+				break;
+			default:
+				break;
+		}
+		
+		return returnValue;
+	}
+	
+}
